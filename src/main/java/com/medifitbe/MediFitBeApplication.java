78990483b482d6e210bdffcc74250ffdb9a942f6
JinHyeon <tiemo0708@naver.com>
2025-05-15 23:48:21 +0900

    chore: clean tracked files using .gitignore

---
+++ src/main/java/com/medifitbe/MediFitBeApplication.java
@@ -0,0 +1,13 @@
+package com.medifitbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediFitBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MediFitBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/medifitbe/MediFitBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.medifitbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediFitBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
