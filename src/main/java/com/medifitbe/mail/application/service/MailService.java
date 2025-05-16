0ead79a4977004ff795560dc150e1e4583d774d2
JinHyeon <tiemo0708@naver.com>
2025-05-17 03:44:14 +0900

    MF-4/feat: 메일 서비스 추가

---
+++ src/main/java/com/medifitbe/mail/application/service/MailService.java
@@ -0,0 +1,22 @@
+package com.medifitbe.mail.application.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MailService {
+
+    private final JavaMailSender mailSender;
+
+    public void sendTo(String to, String subject, String content) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(content);
+        message.setFrom("no-reply@medifit.com");
+        mailSender.send(message);
+    }
+}
--- src/main/java/com/medifitbe/user/adapter/out/persistence/repository/SubscriberRepository.java
+++ src/main/java/com/medifitbe/user/adapter/out/persistence/repository/SubscriberRepository.java
@@ -4,6 +4,9 @@ import com.medifitbe.user.adapter.out.persistence.entity.SubscriberEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface SubscriberRepository extends JpaRepository<SubscriberEntity, Long> {
+    List<SubscriberEntity> findAll();
 }
