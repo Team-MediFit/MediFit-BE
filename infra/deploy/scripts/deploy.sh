e96c2d8e05a15ba19a233412969165b11407cbfd
baekjaehyuk <bjh010321@naver.com>
2025-05-17 16:36:59 +0900

    MF-6/CICD: 배포 스크립트 수정

--- infra/deploy/scripts/deploy.sh
+++ infra/deploy/scripts/deploy.sh
@@ -37,7 +37,7 @@ echo "🛑 기존 컨테이너 중지 중..."
 docker-compose down  # 기존 컨테이너 중지 및 제거
 
 echo "🐳 최신 Docker 이미지 가져오는 중..."
-docker pull tiemo0708/thinkboo-backend-app:latest # 최신 이미지 가져오기
+docker pull jaehyukbaek/medifit-backend-app:latest 최신 이미지 가져오기
 
 echo "🔒 파일 권한 설정 중..."
 chmod 600 /home/ubuntu/.env
